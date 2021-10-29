f3810d372fdb4a34f392d3061a84d76c282a8758
Dr-DOS-6 <92491029+Dr-DOS-6@users.noreply.github.com>
2021-10-30 08:25:24 +0900

    commit on 'dev'

--- cal_ver1.3.5.py
+++ cal_ver1.3.5.py
@@ -278,7 +278,7 @@ def all_calc_code():
                     if cal_n_5 == '0':
                         cal_n_5_1 = float(input('対角線の長さを入力してください。:'))
                         cal_n_6 = float((cal_n_1 + cal_n_2 + cal_n_5_1)/float(2))
-                        area_1 = float(math.sqrt(cal_n_6(cal_n_6 - cal_n_1)(cal_n_6 - cal_n_2)(cal_n_6 - cal_n_5_1)))
+                        area_1 = float(math.sqrt(cal_n_6*(cal_n_6 - cal_n_1)*(cal_n_6 - cal_n_2)*(cal_n_6 - cal_n_5_1)))
                         cal_n_7 = float((cal_n_3 + cal_n_4 + cal_n_5_1)/float(2))
                         area_2 = float(math.sqrt(cal_n_7(cal_n_7 - cal_n_3)(cal_n_7 - cal_n_4)(cal_n_7 - cal_n_5_1)))
                         area_all = str(area_1 + area_2)
