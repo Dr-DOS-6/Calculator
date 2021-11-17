c74e4a3767cba8c4e0c7a1834b54a549066074a7
Dr-DOS-6 <92491029+Dr-DOS-6@users.noreply.github.com>
2021-11-18 08:42:00 +0900

    commit on 'dev'

--- cal_ver1.3.5.py
+++ cal_ver1.3.5.py
@@ -297,14 +297,38 @@ def all_calc_code():
                 elif cal_mode_3_2_1 == '2':
                     cal_n_1 = float(input('1つ目の対角線の長さを入力してください。:'))
                     cal_n_2 = float(input('2つ目の対角線の長さを入力してください。:'))
-                    cal_n_4 = input('θの大きさを選んでください。30°:0 45°:1 60°:2 :')
+                    cal_n_4 = input('θの大きさを選んでください。15°:0 30°:1 45°:2 60°:3 75°:4 90°:5 :')
                     if cal_n_4 == '0':
-                        area = float(0.5*cal_n_1*cal_n_2*0.5)
+                        area = float(0.5*cal_n_1*cal_n_2*(((math.sqrt(6))-(math.sqrt(2)))/4))
                     elif cal_n_4 == '1':
-                        area = float(0.5*cal_n_1*cal_n_2*(math.sqrt(2)/2))
+                        area = float(0.5*cal_n_1*cal_n_2*0.5)
                     elif cal_n_4 == '2':
+                        area = float(0.5*cal_n_1*cal_n_2*(math.sqrt(2)/2))
+                    elif cal_n_4 == '3':
                         area = float(0.5*cal_n_1*cal_n_2*(math.sqrt(3)/2))
+                    elif cal_n_4 == '4':
+                        area = float(0.5*cal_n_1*cal_n_2*(((math.sqrt(2))+(math.sqrt(6)))/4))
+                    elif cal_n_4 == '5':
+                        area = float(0.5*cal_n_1*cal_n_2)
+                    else:
+                        print('深刻なエラーが発生しました。プログラムを再起動します。')
+                        time.sleep(1)
+                        all_calc_code()
                     print('面積:',area)
+                    time.sleep(1)
+                    print('計算を終了します。')
+                    time.sleep(1)
+                    sys.exit()
+                elif cal_mode_3_2_1 == '3':
+                    cal_n_1 = float(input('1つ目の辺の長さを入力してください。:'))
+                    cal_n_2 = float(input('2つ目の辺の長さを入力してください。:'))
+                    cal_n_3 = float(input('3つ目の辺の長さを入力してください。:'))
+                    cal_n_4 = float(input('4つ目の辺の長さを入力してください。:'))
+                    cal_n_5 = input('1つ目の角の大きさを指定してください。15°:0 30°:1 45°:2 60°:3 75°:4 90°:5:')
+                    cal_n_6 = input('2つ目の角の大きさを指定してください。15°:0 30°:1 45°:2 60°:3 75°:4 90°:5:')
+                    #cal_n_7の値と整数の一覧:30°:0 45°:1 60°:2 75°:3 90°:4 105°:5 120°:6 135°:7 150°:8 165°:9 180°:10
+                    if cal_n_5 == '0' and cal_n_6 == '0':
+                        cal_n_7 =  
                 else:
                     print('深刻なエラーが発生しました。プログラムを再起動します。')
                     time.sleep(1)
