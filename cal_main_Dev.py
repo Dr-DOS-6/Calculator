62177f34318926fda29853aeab1d8dc78c29923c
Dr-DOS-6 <92491029+Dr-DOS-6@users.noreply.github.com>
2022-02-08 08:14:12 +0900

    軽微な修正

--- cal_main_Dev.py
+++ cal_main_Dev.py
@@ -127,10 +127,10 @@ error = 'A serious error has occurred. Restarting the program.'
 def startup():
     print('Calculator')
     global soft_ver
-    soft_ver = ('1.4.6.1_CUI_Dev_20220205')
+    soft_ver = ('1.4.6.2_CUI_Dev_20220208')
     str(soft_ver)
     if argv == 'debug':
-        soft_ver = ('1.4.6.1_CUI_Dev_20220205'+' '+'debug_mode')
+        soft_ver = ('1.4.6.2_CUI_Dev_20220208'+' '+'debug_mode')
     #Hallo 2022, Happy new year!!
     ver = 'Version'+' '+soft_ver
     #体積計算モード、表面積計算モードをモード2に統合
