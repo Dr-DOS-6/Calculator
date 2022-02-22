ea39ad0b1ea3b0fcc5a713a644cd02edc0877dab
Dr-DOS-6 <92491029+Dr-DOS-6@users.noreply.github.com>
2022-02-22 12:26:30 +0900

    製品版更新

--- cal_main.py
+++ cal_main.py
@@ -134,10 +134,10 @@ error = 'A serious error has occurred. Restarting the program.'
 def startup():
     print('Calculator')
     global soft_ver
-    soft_ver = ('4.0')
+    soft_ver = ('5.0')
     str(soft_ver)
     if argv == 'debug':
-        soft_ver = ('3.1'+' '+'debug_mode')
+        soft_ver = ('5.0'+' '+'debug_mode')
     #Hallo 2022, Happy new year!!
     ver = 'Version'+' '+soft_ver
     #体積計算モード、表面積計算モードをモード2に統合
@@ -155,6 +155,7 @@ def startup():
     #可読性の向上
     #time.sleep()の全削除
     #税計算モード完成
+    #数値変換モード半完成
     #数値変換モード完成
     builder = 'Dr.DOS'
     year = '2021'
@@ -540,12 +541,12 @@ def all_calc_code():
                 ans_2 = 'tan'
             else:
                 error_end('0x0001',None)
-            deg_sel_1 = int(input('角度を入力してください。'))
-            deg_sel_2 = li.deg.index(deg_sel_1)
-            if not 0 <= deg_sel_2 < len(li.deg):
+            sel_str = str(li.deg)+' '+'左のリストの中から角度を選んで入力してください。それぞれ左から0,1,2,3,4,5,6,7,8です。:'
+            deg_sel_1 = int(input(sel_str))
+            if not 0 <= deg_sel_1 < len(li.deg):
                 error_end('0x0001',None)
-            ans = tri[deg_sel_2]
-            ans_3 = str(deg_sel_1)
+            ans = tri[deg_sel_1]
+            ans_3 = str(li.deg[deg_sel_1])
             input_ = ans_2+','+ans_3
         elif sel == 1:
             tri = int(input('角度へ変換する物を選んでください。sin:0 cos:1 tan:2 '))
@@ -559,18 +560,17 @@ def all_calc_code():
                 tri = li.tantheta
                 ans_2 = 'tan'
             else:error_end('0x0001',None)
-            tri_str = str(tri)+' '+'左のリストの中から値を選んで入力してください。'
-            val_sel_1 = str(input(tri_str))
-            val_sel_2 = tri.index(val_sel_1)
-            ans_3 = tri[val_sel_2]
-            if not 0 <= val_sel_2 < len(tri):
+            tri_str = str(tri)+' '+'左のリストの中から値を選んで入力してください。それぞれ左から0,1,2,3,4,5,6,7,8です。:'
+            val_sel_1 = int(input(tri_str))
+            ans_3 = tri[val_sel_1]
+            if not 0 <= val_sel_1 < len(tri):
                 error_end('0x0001',None)
-            ans = str(li.deg[val_sel_2])
-            input_ = ans_2+','+val_sel_1
+            ans = str(li.deg[val_sel_1])
+            input_ = ans_2+','+ans_3
         if sel == 0: 
             print(ans_2+ans_3+'°','=',ans)
         elif sel == 1:
-            print(ans_2,',',ans_3,'=',ans+'°')
+            print(ans_3,'=',ans_2+ans+'°')
         output_6(input_,ans)
         end()
     elif cal_mode == '4':
