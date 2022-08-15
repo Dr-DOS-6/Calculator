beaae3283ea31bd9d81ea2b693357b78dc5ae134
Dr.GLaDOS <akkun6125@ab.auone-net.jp>
2022-08-15 09:26:28 +0900

    #

--- Easy-to-Use_GUI_Calculator/C++/main.cpp
+++ Easy-to-Use_GUI_Calculator/C++/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <string>
+using namespace std;
 int main(){
-    int a = 256;
-    int b = 128;
-    int c = a + b;
+    cout << "Please input your name." << endl;
+    string ipt1;
+    cin >> ipt1;
+    cout << "Hello, " << ipt1 << ". what do you want to do?"<<endl;
+    cout << " 1:Mode 1 2:Mode 2" << endl;
+    string ipt2;
+    cin >> ipt2;
+    if(ipt2 == "1") cout << "Mode 1 selected." << endl;
+    else if (ipt2 == "2") cout << "Mode 2 selected." << endl;
 }
